dbfcf773e5f972500bc8a9a313458c80f1484957
Tiago Ribeiro <1181444@isep.ipp.pt>
2021-05-12 22:08:12 +0100

    Cart button bump + details

--- Food_Ordering_Application/src/components/Layout/HeaderCartButton.js
+++ Food_Ordering_Application/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     //HeaderCartButton will be re-evaluated by React whenever the context changes
     const cartCtx = useContext(CartContext); //to get access to this cart context which is managed by the closest provider (on App)
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return(
         <React.Fragment>
-            <button className={classes.button} onClick={props.onClick}>
+            <button className={btnClasses} onClick={props.onClick}>
                 <span className={classes.icon}>
                     <CartIcon />
                 </span>
